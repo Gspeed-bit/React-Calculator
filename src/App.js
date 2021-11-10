2115ae151d957407f659285a0d647bb4fe578b48
Your Name <adeyeye17@gmail.com>
2021-11-11 00:05:05 +0100

    At this stage i was only able to type in the numbers only and i also implemented the reset button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 
 import './App.css';
 
-import {useSate, useEffect} from "react"
+import {useState, useEffect} from "react"
 
 
 function App() {
 
-  const numInput = (e) =>{
+  const [val, setVal ] = useState("0") //the display value
+  const [iniState, setIniState ] = useState("")
+  const [finState, setFinState ] = useState("")
+  const [calOperator, setCalOperator ] = useState(null)
+  const [result, setResult ] = useState(false)
 
+
+  const numInput = (e) =>{
+      if (finState.includes(".") && e.target.innerText === ".") return; // this wont allow the dot button to be clicked twice
+
+      if(result){
+        setIniState("") // we set it to nothing because we want the cal to know that once we press the result/equal to we dont want it to continue calculating underground
+      }
+
+      finState ? 
+      setFinState ((pre) => pre + e.target.innerText) : 
+      setFinState(e.target.innerText)
+      //  i did this because i want to be able to click a button twice and get the value
+      setResult(false)
   };
+
+useEffect(()=>{
+
+  setVal("0")
+  
+}, [])
+
+useEffect(()=>{
+setVal(finState)
+  
+}, [finState])
+
   const operatorType = (e) =>{
 
   };
@@ -22,20 +51,14 @@ function App() {
 
   };
   const reset = () =>{
-
+      setVal("0")
   };
-
-
-
-
-
-
 
 
   return (
     <div className="container">
       <div className= "wrapper">
-    <div className="screen"></div>
+    <div className="screen">{val}</div>
     <div className="greys btn" onClick={reset}> AC </div>
     <div className="greys btn" onClick={percentage}> % </div>
     <div className="greys btn" onClick={plusMinus}> +/- </div>
