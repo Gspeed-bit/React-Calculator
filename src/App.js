d2215042ec505ab95a9c15ecbaf94abe161e18a5
Your Name <adeyeye17@gmail.com>
2021-11-12 13:16:00 +0100

    second to the last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
-import './App.css';
-
 import {useState, useEffect} from "react"
+import NumberFormat from "react-number-format"
 
 
 function App() {
@@ -14,41 +13,85 @@ function App() {
 
 
   const numInput = (e) =>{
-      if (finState.includes(".") && e.target.innerText === ".") return; // this wont allow the dot button to be clicked twice
+      if (finState.includes(".") && e.target.innerText === ".") return; // this wont allow the dot button to be clicked twice i.e we cant have two dot sign 
 
       if(result){
-        setIniState("") // we set it to nothing because we want the cal to know that once we press the result/equal to we dont want it to continue calculating underground
+        setIniState("0") // we set it to nothing because we want the cal to know that once we press the result/equal to we dont want it to continue calculating underground
       }
 
       finState ? 
-      setFinState ((pre) => pre + e.target.innerText) : 
-      setFinState(e.target.innerText)
-      //  i did this because i want to be able to click a button twice and get the value
-      setResult(false)
+      setFinState ((pre) => pre + e.target.innerText) :    //  i did this because i want to be able to click a button twice and get the value
+      setFinState(e.target.innerText) // i will be able to input numbers
+   
+      setResult(false) // @ this stage the calculator should not perform any function yet 
   };
 
+  // component did mount
 useEffect(()=>{
 
   setVal("0")
   
 }, [])
 
+
+//component did update
 useEffect(()=>{
 setVal(finState)
   
 }, [finState])
 
   const operatorType = (e) =>{
-
+    setResult(false);
+    setCalOperator(e.target.innerText)
+    if ( finState === "") return
+    if(iniState !== "") {
+      equals()
+    }else{
+  setIniState(finState)
+    setFinState("")
+  }
   };
-  const equals = (e) =>{
 
+
+
+  const equals = (e) => {
+    if (e?.target.innerText === "=") {
+      setResult(true);
+    }
+    let cal;
+    switch (calOperator) {
+      case "/":
+        cal = String(parseFloat(iniState) / parseFloat(finState));
+        break;
+
+      case "+":
+        cal = String(parseFloat(iniState) + parseFloat(finState));
+        break;
+      case "x":
+        cal = String(parseFloat(iniState) * parseFloat(finState));
+        break;
+      case "-":
+        cal = String(parseFloat(iniState) - parseFloat(finState));
+        break;
+      default:
+        return;
+    }
+    setVal("");
+    setIniState(cal);
+    setFinState("");
   };
   const percentage = () =>{
+    iniState ? 
+    setFinState(String((parseFloat(finState) / 100) * iniState)) :
+    setFinState(String((parseFloat(finState) / 100)))
 
   };
   const plusMinus = () =>{
-
+    if (finState.charAt(0) === "-") {
+      setFinState(finState.substring(1));
+    } else {
+      setFinState("-" + finState);
+    }
   };
   const reset = () =>{
       setVal("0")
@@ -60,7 +103,8 @@ setVal(finState)
   return (
     <div className="container">
       <div className= "wrapper">
-    <div className="screen">{val}</div>
+    <div className="screen">{val !== "" || val === "0" ? <NumberFormat value= {val} displayType= {"text"} thousandSeparator ={true} /> : <NumberFormat value = {iniState} displayType = {"text"} thousandSeparator = {true} />}
+    </div>
     <div className="greys btn" onClick={reset}> AC </div>
     <div className="greys btn" onClick={percentage}> % </div>
     <div className="greys btn" onClick={plusMinus}> +/- </div>
